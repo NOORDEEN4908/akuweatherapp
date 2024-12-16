37794f9a31ceb369ab40ee8119bf657c53a2901f
Dulanga Dasanayake <dulanganiroshan721@gmail.com>
2024-12-16 16:26:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.css'
 import Home from './Pages/Home/Home'
+import WeatherBox from "./CurrentWeather";
 
 const App = () => {
   return (
     <div>
       <Home />
+      <CurrentWeather />
     </div>
   )
 }

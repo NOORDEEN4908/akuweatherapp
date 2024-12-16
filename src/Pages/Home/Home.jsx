2079b8336621db96c5f185f8738cb4259250f703
NOORDEEN4908 <nd712562@gmail.com>
2024-12-16 15:43:00 +0530

    Create a related componanents and home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure reprehenderit, voluptatem officiis at suscipit nulla corporis veniam expedita sapiente, porro quod ipsa dolores architecto provident consequatur. Assumenda totam eum iste.</p>
+    </div>
+  )
+}
+
+export default Home

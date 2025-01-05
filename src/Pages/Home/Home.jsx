35fb4e1504fde53d023edf2b5d45e7dc289e15e7
Dulanga Dasanayake <dulanganiroshan721@gmail.com>
2025-01-06 00:08:29 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import CurrentWeather from '../../Components/CurrentWeather/CurrentWeather'
-
-
-
+import WeatherGraphs from '../../Components/WeatherGraphs/WeatherGraphs'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
       <CurrentWeather />
-        
-     
-     
+        <WeatherGraphs />
       </div>
   )
 }
